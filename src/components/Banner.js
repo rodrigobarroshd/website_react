ec691c64217cca64522480ff200c6724d81d94fb
Rodrigo Barros <brodrigo87@gmail.com>
2022-09-01 11:31:03 -0300

    toRotate change

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 200);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "MindMaster", "Pensamento Computacional", "UI/UX Designer" ];
+  const toRotate = [ "MindMaster", "Pensamento Computacional", "Computação Desplugada" ];
   const period = 2000;
 
   useEffect(() => {
@@ -55,10 +55,10 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to my Portfolio</span>
+                <span className="tagline">Bem Vindo ao MindMaster</span>
                 <h1>{` `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Bem Vindo ao MindMaster", "Pensamento Computacional", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                  <button onClick={() => console.log('connect')}>Entre em Contato <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
           </Col>
