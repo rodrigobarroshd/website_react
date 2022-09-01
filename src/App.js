17c71a0be70b3cc91d24f199988d6a3ec0c794d4
Rodrigo Barros <brodrigo87@gmail.com>
2022-09-01 11:30:33 -0300

    about section

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import  { About }  from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
-      <Skills />
+      <Skills /> 
+      <About/>
       <Projects />
       <Contact />
       <Footer />
