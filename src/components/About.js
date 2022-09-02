526c7ddeca33cb5921a49a5256e73d68f0a25ae6
Rodrigo Barros <brodrigo87@gmail.com>
2022-09-02 17:30:41 -0300

    about

--- src/components/About.js
+++ src/components/About.js
@@ -42,14 +42,21 @@ export const About = () => {
             <div className="text-right-items-center-text-center">
               <h1 className="title-font-text-white">
                 Olá, sou a Renata.
-                <br className="taglinehd" />Lorem ipsum dolor sit amet, consectetur.
+                <br className="taglinehd" />
               </h1>
               <p className="leading-relaxed">
               Renata Souza Barreto é um exemplo claro de estudante que adquiriu amplas possibilidades e 
               liberdades para escolher caminhos que pudessem seguir, sendo PROTAGONISTA da sua trajetória, 
-              aqui no Colégio Perfil. Os estudantes do Ensino Fundamental, 
-              iniciarão os estudos com o componente curricular de Pensamento Computacional, 
-              sob a tutoria da professora Renata, que também é ex aluna do Perfil.
+              aqui no Colégio Perfil. 
+              
+              </p>
+
+              <p className="leading-relaxed">
+              Doutoranda em Modelagem Computacional e Tecnologia Industrial (PPGMCTI) SENAI-CIMATEC; 
+              Mestranda no Programa de Pós-Graduação em Ensino, Filosofia e História das Ciências 
+              – UFBA; Mestre em Modelagem Computacional e Tecnologia Industrial (PPGMCTI) SENAI-CIMATEC; 
+              Graduada em Pedagogia pela Universidade do Estado da Bahia; Graduada em Educação Física 
+              Licenciatura pela União Metropolitana de Educação e Cultura.
               </p>
             <div className="flex justify-center">
               <a
